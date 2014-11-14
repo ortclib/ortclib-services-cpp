e972913ffd5433d477d8c7820cc5bd260075e9d8
Robin Raymond <robin@hookflash.com>
2014-11-14 12:11:29 -0500

    - added tests for domain validation

--- openpeer/services/test/TestHelper.cpp
+++ openpeer/services/test/TestHelper.cpp
@@ -40,14 +40,10 @@
 
 using zsLib::String;
 
-ZS_DECLARE_TYPEDEF_PTR(openpeer::services::IHelper, UseHelepr)
+ZS_DECLARE_TYPEDEF_PTR(openpeer::services::IHelper, UseHelper)
 
-void doTestHelper()
+static void testI18NIDN()
 {
-  if (!OPENPEER_SERVICE_TEST_DO_HELPER_TEST) return;
-
-  BOOST_INSTALL_LOGGER();
-
   wchar_t rawInput1[] = {0x0077, 0x0077, 0x0077, 0x002E, 0x65E5, 0x672C, 0x5E73, 0x002E, 0x006A, 0x0070, 0x00};
   wchar_t rawInput2[] = {0x0077, 0x0077, 0x0077, 0x002E, 0x30CF, 0x30F3, 0x30C9, 0x30DC, 0x30FC, 0x30EB, 0x30B5, 0x30E0, 0x30BA, 0x002E, 0x0063, 0x006F, 0x006D, 0x00};
   wchar_t rawInput3[] = {0x0077, 0x0077, 0x0077, 0x002E, 0x0066, 0x00E4, 0x0072, 0x0067, 0x0062, 0x006F, 0x006C, 0x0061, 0x0067, 0x0065, 0x0074, 0x002E, 0x006E, 0x0075, 0x00};
@@ -60,51 +56,101 @@ void doTestHelper()
   wchar_t rawInput10[] = {0x0077, 0x0077, 0x0077, 0x002E, 0x0066, 0x00E4, 0x0072, 0x006A, 0x0065, 0x0073, 0x0074, 0x0061, 0x0064, 0x0073, 0x0062, 0x006B, 0x002E, 0x006E, 0x0065, 0x0074, 0x00};
   wchar_t rawInput11[] = {0x0077, 0x0077, 0x0077, 0x002E, 0x006D, 0x00E4, 0x006B, 0x0069, 0x0074, 0x006F, 0x0072, 0x0070, 0x0070, 0x0061, 0x002E, 0x0063, 0x006F, 0x006D, 0x00};
 
-  wchar_t *rawInputs[] = {
-    rawInput1,
-    rawInput2,
-    rawInput3,
-    rawInput4,
-    rawInput5,
-    rawInput6,
-    rawInput7,
-    rawInput8,
-    rawInput9,
-    rawInput10,
-    rawInput11,
-    NULL
+  struct I18NConvert {
+    wchar_t *mInput;
+    const char *mOutput;
   };
 
-  const char *expectedOutputs[] = {
-    "www.xn--gwtq9nb2a.jp",
-    "www.xn--vckk7bxa0eza9ezc9d.com",
-    "www.xn--frgbolaget-q5a.nu",
-    "www.xn--bcher-kva.de",
-    "www.xn--brndendekrlighed-vobh.com",
-    "www.xn--rksmrgs-5wao1o.se",
-    "www.xn--9d0bm53a3xbzui.com",
-    "xn--lck1c3crb1723bpq4a.com",
-    "xn--l8je6s7a45b.com",
-    "www.xn--frjestadsbk-l8a.net",
-    "www.xn--mkitorppa-v2a.com",
-    NULL
+  I18NConvert rawInputs[] = {
+    { rawInput1, "www.xn--gwtq9nb2a.jp" },
+    { rawInput2, "www.xn--vckk7bxa0eza9ezc9d.com" },
+    { rawInput3, "www.xn--frgbolaget-q5a.nu" },
+    { rawInput4, "www.xn--bcher-kva.de" },
+    { rawInput5, "www.xn--brndendekrlighed-vobh.com" },
+    { rawInput6, "www.xn--rksmrgs-5wao1o.se" },
+    { rawInput7, "www.xn--9d0bm53a3xbzui.com" },
+    { rawInput8, "xn--lck1c3crb1723bpq4a.com" },
+    { rawInput9, "xn--l8je6s7a45b.com" },
+    { rawInput10, "www.xn--frjestadsbk-l8a.net" },
+    { rawInput11, "www.xn--mkitorppa-v2a.com" },
+    {NULL, NULL}
   };
 
-  for (size_t loop = 0; NULL != rawInputs[loop]; ++loop)
+  for (size_t loop = 0; NULL != rawInputs[loop].mInput; ++loop)
   {
-    wchar_t *rawInput = rawInputs[loop];
-    String expecting(expectedOutputs[loop]);
+    wchar_t *rawInput = rawInputs[loop].mInput;
+    String expecting(rawInputs[loop].mOutput);
 
     String input(rawInput);
 
-    String output = UseHelepr::convertUTF8ToIDN(input);
+    String output = UseHelper::convertUTF8ToIDN(input);
 
     BOOST_EQUAL(expecting, output)
 
-    String backToOriginal = UseHelepr::convertIDNToUTF8(output);
+    String backToOriginal = UseHelper::convertIDNToUTF8(output);
 
     BOOST_EQUAL(input, backToOriginal)
+
+    bool validated = UseHelper::isValidDomain(input);
+
+    BOOST_CHECK(validated)
     
   }
+}
+
+static void testDomainValidation()
+{
+  struct ValidateDomain
+  {
+    const char *mDomain;
+    bool mWillValidate;
+  };
+
+  ValidateDomain validateDomains[] = {
+    {"bogus", false},
+    {"valid.com", true},
+    {"invalid..com", false},
+    {"-invalid.com", false},
+    {"prefix.valid.com", true},
+    {"prefix.invalid.com.", false},
+    {".prefix.invalid.com", false},
+    {".invalid.com", false},
+    {"_invalid.com", false},
+    {"prefix_invalid.com", false},
+    {"prefix-valid.com", true},
+    {"prefix--invalid.com", false},
+    {"valid.com", true},
+    {"valid.technology", true},
+    {"valid.abcdefghijklmnopqrstuvwxyz", true},
+    {"valid.123456789012345678901234567890123456789012345678901234567890123", false},
+    {"valid.abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk", true},
+    {"invalid.abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijkl", false},
+    {"valid.abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.valid", true},
+    {"invalid.abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijkl.invalid", false},
+    {"invalid.d", false},
+
+    {NULL, false}
+  };
+
+  for (int loop = 0; NULL != validateDomains[loop].mDomain; ++loop) {
+    const char *domain = validateDomains[loop].mDomain;
+    bool validated = UseHelper::isValidDomain(domain);
+
+    BOOST_EQUAL(validated, validateDomains[loop].mWillValidate)
+    if (validated != validateDomains[loop].mWillValidate) {
+      BOOST_EQUAL(domain, "failed")
+    }
+  }
+  
+}
+
+void doTestHelper()
+{
+  if (!OPENPEER_SERVICE_TEST_DO_HELPER_TEST) return;
+
+  BOOST_INSTALL_LOGGER();
+
+  testI18NIDN();
+  testDomainValidation();
 
 }
